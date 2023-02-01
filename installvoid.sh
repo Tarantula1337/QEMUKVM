c689c2b8cb0744487dfe8ac98cab701c7f4c1d06
Tarantula1337 <42551966+Tarantula1337@users.noreply.github.com>
2023-02-01 12:24:37 +0100

    Create installvoid.sh

---
+++ installvoid.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+xbps-install qemu libvirt virt-manager bridge-utils iptables
+ln -s /etc/sv/dbus /var/service
+ln -s /etc/sv/libvirtd /var/service
+ln -s /etc/sv/virtlockd /var/service
+ln -s /etc/sv/virtlogd /var/service
